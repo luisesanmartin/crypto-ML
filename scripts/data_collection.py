ca99d5fb745cd6c7cb6a5768cccb6a6735008214
Luis Eduardo San Martin <luise.sanmartin@gmail.com>
2024-11-19 19:32:37 -0500

    added function to fetch data with start/end limits

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -4,11 +4,14 @@ sys.path.insert(1, './utils')
 import utils_fetch
 import objects
 
-data = utils_fetch.get_data_max()
-data = data[1:] #drop first obs as it's going to be incomplete
+time_start = '2024-09-07T20:00:00.000'
+time_end = '2024-11-19T23:00:00.000'
+limit = 11000
+data = utils_fetch.get_data_with_limits(time_start, time_end, limit)
 
-end = data[0]['time_period_end'].split('.')[0]
-start = data[-1]['time_period_end'].split('.')[0]
+
+end = data[-1]['time_period_end'].split('.')[0]
+start = data[0]['time_period_end'].split('.')[0]
 frequency = objects.PERIOD_DATA
 
 file = '../data/raw/data_{}_{}_{}_{}.pkl'.format(objects.SYMBOL_ID, frequency, start, end)
