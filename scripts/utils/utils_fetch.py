1456ac3af2726315290819fbcf0635496e583783
Luis Eduardo San Martin <luise.sanmartin@gmail.com>
2024-11-19 19:39:44 -0500

    discarded remote branch added by mistake

--- scripts/utils/utils_fetch.py
+++ scripts/utils/utils_fetch.py
@@ -33,27 +33,7 @@ def get_data_max(n=objects.DATA_MAX, period=objects.PERIOD_DATA, crypto_symbol=o
     request_url = objects.URL.format(crypto_symbol)
     headers = {"X-CoinAPI-Key": get_api_key()}
     parameters = {
-        'period_id': period,
-        'limit': n
-    }
-
-    response = requests.get(request_url, headers=headers, params=parameters)
-    data = json.loads(response.text)
-
-    return data
-
-def get_data_with_limits(start, end, n=objects.DATA_MAX, period=objects.PERIOD_DATA, crypto_symbol=objects.SYMBOL_ID):
-
-    '''
-    Retrieves data for a crypto, with a frequency of 'period', from 'start' to 'end'
-    '''
-
-    request_url = objects.URL.format(crypto_symbol)
-    headers = {"X-CoinAPI-Key": get_api_key()}
-    parameters = {
-        'period_id': period,
-        'time_start': start,
-        'time_end': end,
+        'period_id': objects.PERIOD_DATA,
         'limit': n
     }
 
