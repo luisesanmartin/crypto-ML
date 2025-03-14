97156ae7a64f8992ed42a154f8a0d1fd65e08767
Luise <38444484+luisesanmartin@users.noreply.github.com>
2025-03-14 09:30:39 -0400

    quickfix: amount

--- scripts/trader.py
+++ scripts/trader.py
@@ -12,7 +12,7 @@ def main():
 
 	# Globals and variables
 	symbols = objects.BITSTAMP_SYMBOLS
-	amount = 100
+	amount = 200
 	fee_rate = objects.FEE_RATE
 	buy_rate = objects.BUY_RATE
 	sell_rate = objects.SELL_RATE
