b27d53d96c2c92ed32957d487d592d5f48210a96
Luis Eduardo San Martin <luise.sanmartin@gmail.com>
2026-03-30 14:00:20 -0500

    deleted unused scripts and some functions not used in ML pipeline

--- scripts/utils/utils_fetch.py
+++ scripts/utils/utils_fetch.py
@@ -5,13 +5,6 @@ import utils_time
 import utils_data
 import utils_features
 
-def get_api_key(text='API/key.txt'):
-
-    with open(text) as file:
-        key = file.read()
-
-    return key
-
 def get_data_bitstamp_from_to(
     start,
     end,
@@ -37,26 +30,6 @@ def get_data_bitstamp_from_to(
 
     return result
 
-def get_data_bitstamp_symbols_now(
-    step = objects.GAP_EPOCH,
-    symbols = objects.BITSTAMP_SYMBOLS,
-    limit = objects.PERIOD+1
-    ):
-
-    '''
-    Return a dictionary with the current prices and average prices
-    for the last limit-1 periods for all symbols
-    '''
-
-    all_data = {symbol: get_data_bitstamp(step=step, crypto_symbol=symbol, limit=limit) for symbol in symbols}
-    data_dic = utils_data.make_data_dic_bitstamp(all_data)
-    time = list(data_dic.keys())[-1]
-
-    current_prices = {symbol: float(data_dic[time][symbol]['close']) for symbol in symbols}
-    avg_prices = {symbol: utils_features.avg_price_symbol_periods(data_dic, symbol, limit-1, time) for symbol in symbols}
-
-    return current_prices, avg_prices
-
 def get_data_bitstamp(
     step = 300, # five minutes
     crypto_symbol=objects.SYMBOL_ID_BITSTAMP,
@@ -77,38 +50,5 @@ def get_data_bitstamp(
 
     response = requests.get(request_url, params=parameters)
     data = json.loads(response.text)['data']['ohlc']
-
-    return data
-
-def get_data_min(period=objects.PERIOD_DATA, crypto_symbol=objects.SYMBOL_ID):
-
-    '''
-    Retrieves data for a crypto, with a frequency of 'period'
-    '''
-
-    request_url = objects.URL.format(crypto_symbol)
-    headers = {"X-CoinAPI-Key": get_api_key()}
-    parameters = {'period_id': objects.PERIOD_DATA}
-
-    response = requests.get(request_url, headers=headers, params=parameters)
-    data = json.loads(response.text)
-
-    return data
-
-def get_data_max(n=objects.DATA_MAX, period=objects.PERIOD_DATA, crypto_symbol=objects.SYMBOL_ID):
-
-    '''
-    Retrieves data for a crypto, with a frequency of 'period'
-    '''
-
-    request_url = objects.URL.format(crypto_symbol)
-    headers = {"X-CoinAPI-Key": get_api_key()}
-    parameters = {
-        'period_id': objects.PERIOD_DATA,
-        'limit': n
-    }
-
-    response = requests.get(request_url, headers=headers, params=parameters)
-    data = json.loads(response.text)
 
     return data
